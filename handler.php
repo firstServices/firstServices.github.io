75eb1a26d991367fbbf5eefc396938e7c4a053c9
Linda Ting <lstting@seas.upenn.edu>
2020-07-05 18:35:14 -0400

    change send to email

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('comments')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('lstting@seas.upenn.edu'); // ← Your email here
+$pp->sendEmailTo('customerrelations@psafirstservices.com'); // ← Your email here
 
 echo $pp->process($_POST);
