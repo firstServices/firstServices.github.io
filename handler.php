0f23f47edbe2e1408128de82af6286437fb726a7
Linda Ting <lstting@seas.upenn.edu>
2020-07-05 18:21:04 -0400

    to test formpage

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('someone@gmail.com'); // ← Your email here
+$pp->sendEmailTo('lstting@seas.upenn.edu'); // ← Your email here
 
 echo $pp->process($_POST);
